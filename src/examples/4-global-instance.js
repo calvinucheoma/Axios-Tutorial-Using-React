052462e5e95c6f62f4bb375e8736785977f3ab67
Calvin <calvinchukwuma@gmail.com>
2022-04-08 10:43:16 +0100

    Add files via upload

---
+++ src/examples/4-global-instance.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const productsUrl = 'https://course-api.com/react-store-products';
+const randomUserUrl = 'https://randomuser.me/api';
+
+const GlobalInstance = () => {
+  const fetchData = async () => {
+    try {
+      const resp1 = await axios(productsUrl);
+      const resp2 = await axios(randomUserUrl);
+    }
+    catch(error){
+      console.log(error.resp1);
+      console.log(error.resp2);
+    }
+    console.log('global axios instance');
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <h2 className='text-center'>global instance</h2>;
+};
+export default GlobalInstance;
